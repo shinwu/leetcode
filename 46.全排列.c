ee144b024ac02f94c0c42313c51cf4f086634f66
shinwu <shinwu.chen@gmail.com>
2021-12-19 11:22:54 +0800

    initial commit

---
+++ 46.全排列.c
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode.cn id=46 lang=c
+ *
+ * [46] 全排列
+ */
+
+// @lc code=start
+
+
+/**
+ * Return an array of arrays of size *returnSize.
+ * The sizes of the arrays are returned as *returnColumnSizes array.
+ * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
+ */
+int** permute(int* nums, int numsSize, int* returnSize, int** returnColumnSizes){
+
+}
+// @lc code=end
+
